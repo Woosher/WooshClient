e1c36bb0873973ddb9b44fdc6f1e2bf0d21bb5a2
Felza <pet_nysen@yaho.dk>
2017-09-27 14:18:56 +0200

    JavaFX

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,40 @@
-import controllers.FlowController;
-import entities.DeploymentPackage;
-import entities.ResultsListener;
-import entities.parsing.Node;
+package view;
 
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Main {
-
+public class Main extends Application{
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     * <p>
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("sample.fxml"));
+        BorderPane root = new BorderPane();
+        root.setCenter(loader.load());
+        loader.getController();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
 
     public static void main(String[] args){
-        FlowController flowController = new FlowController();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("After Starting thread");
-        while(scanner.hasNext()){
-            String line = scanner.next();
-            if(line.equals("k")){
-                startStuff(flowController);
-            }
-            System.out.println(line);
-        }
-
+        launch(args);
     }
-
-    public static void startStuff(FlowController flowController){
-        System.out.println("Starting thread");
-        flowController.sendPackage(null, new ResultsListener<String>() {
-            public void onCompletion(String result) {
-                System.out.println(result);
-            }
-
-            public void onFailure(Throwable throwable) {
-                System.out.println(throwable.getMessage());
-            }
-        });
-    }
-
 }
