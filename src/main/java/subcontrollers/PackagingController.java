fe82a7c6cd986c635e36baeb48c1bc358d4d0fd1
Felza <pet_nysen@yaho.dk>
2017-09-28 15:57:53 +0200

    Compressing

--- src/main/java/subcontrollers/PackagingController.java
+++ src/main/java/subcontrollers/PackagingController.java
@@ -1,7 +1,9 @@
 package subcontrollers;
 
 
+import entities.parsing.LoadBalancer;
 import entities.parsing.Machine;
+import entities.parsing.Node;
 import exceptions.WooshException;
 import iohelpers.Scripter;
 import iohelpers.interfaces.ScripterInterface;
@@ -19,9 +21,14 @@ public class PackagingController implements PackagingInterface {
         return null;
     }
 
-    public String compressPackage(Machine machine) throws WooshException {
-        scripter.compressPackage(null, null,null,null);
-        return null;
+    @Override
+    public String compressPackage(Node node) throws WooshException {
+        scripter.compressPackage(node.getPathBash(), node.getPath(),"/home/Toby/packages/" ,node.getName());
+    }
+
+    @Override
+    public String compressPackage(LoadBalancer loadBalancer) throws WooshException {
+        scripter.compressPackage(loadBalancer.getPathBash(), null,"/home/Toby/packages/" ,loadBalancer.getName());
     }
 
 }
--- src/main/java/subcontrollers/interfaces/PackagingInterface.java
+++ src/main/java/subcontrollers/interfaces/PackagingInterface.java
@@ -1,6 +1,8 @@
 package subcontrollers.interfaces;
 
+import entities.parsing.LoadBalancer;
 import entities.parsing.Machine;
+import entities.parsing.Node;
 import exceptions.WooshException;
 
 public interface PackagingInterface {
@@ -8,6 +10,8 @@ public interface PackagingInterface {
 
     String createBashScripts(Machine machine) throws WooshException;
 
-    String compressPackage(Machine machine) throws WooshException;
+    String compressPackage(Node node) throws WooshException;
+
+    String compressPackage(LoadBalancer loadBalancer) throws WooshException;
 
 }
